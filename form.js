09775d0580d14aa676d75b2765dce1418a69ae40
mik8920 <daal.mikaela@gmail.com>
2024-02-08 14:56:51 +0200

    form

---
+++ form.js
@@ -0,0 +1,22 @@
+const submit = document.querySelector(".submitBtn");
+const login = document.querySelector(".logInBtn");
+const form = document.querySelector("#myform");
+
+const verifyPassword = (e) => {
+  const passInput = document.querySelector("#pwd");
+  const conpassInput = document.querySelector("#conpwd");
+
+  if (passInput.value !== conpassInput.value) {
+    conpassInput.setCustomValidity("*Passwords do not match");
+    passInput.setCustomValidity("");
+    e.preventDefault();
+  } else if (passInput.value === "" || conpassInput.value === "") {
+    passInput.setCustomValidity("This field is required");
+    conpassInput.setCustomValidity("");
+    e.preventDefault();
+  }
+  /*   conpassInput.reportValidity();
+   */
+};
+
+myform.addEventListener("submit", verifyPassword);
